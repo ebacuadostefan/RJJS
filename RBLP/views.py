1d0017cb0668a984c94a419ce73535a363550753
Hello world <ebacuadomentino@gmail.com>
2024-05-30 13:58:20 +0800

    Latest lesson

--- RBLP/views.py
+++ RBLP/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Gender
+from django.shortcuts import redirect, render
+from .models import Gender, User
 from django.contrib import messages
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
@@ -22,3 +23,85 @@ def store_gender(request):
     messages.success(request, 'Gender successfully saved!')
     
     return redirect('/genders')
+
+def show_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id)# SELECT *FROM genders WHERE gender_id = gender_id
+    
+    context = {
+        'gender': gender,
+    }
+    
+    return render(request, 'gender/show.html', context)
+
+def edit_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id)# SELECT *FROM genders WHERE gender_id = gender_id
+    
+    context = {
+        'gender': gender,
+    }
+    return render(request, 'gender/edit.html', context)
+
+def update_gender(request, gender_id):
+    gender = request.POST.get('gender')
+    
+    Gender.objects.filter(pk=gender_id).update(gender=gender) #UPDATE genders SET gender = gender WHERE gender)id = gender_id
+    messages.success(request, 'Gender succesfully updated')
+    return redirect('/genders')
+
+
+def delete_gender(request,gender_id):
+    gender = Gender.objects.get(pk=gender_id)# SELECT *FROM genders WHERE gender_id = gender_id
+    
+    context = {
+        'gender': gender,
+    }
+    return render(request, 'gender/delete.html', context)
+
+def destroy_gender(request, gender_id):
+    Gender.objects.filter(pk=gender_id).delete()# DELETE FROM gender WHERE gender_id = gender_id
+    messages.success(request,"Gender successfully deleted.")
+    
+    return redirect('/genders')
+
+def index_user(request):
+    user = User.objects.select_related()
+    
+    context = {
+        'users': user,
+    }
+    
+    return render(request, 'user/index.html', context)
+
+def create_user(request):
+    genders = Gender.objects.all() #SELECT * FROM genders
+    
+    context = {
+        'genders': genders
+    }
+    
+    return render(request, 'user/create.html', context)
+
+def store_user(request):
+    firstName = request.POST.get('first_name')
+    middleName = request.POST.get('middle_name')
+    lastName = request.POST.get('last_name')
+    age = request.POST.get('age')
+    birthDate = request.POST.get('birth_date')
+    genderId = request.POST.get('gender_id')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    confirmPassword = request.POST.get('confirm_password')
+    
+    if password == confirmPassword:
+        encryptedPassword = make_password(password)
+        
+        User.objects.create(first_name=firstName, middle_name=middleName, last_name=lastName, age=age, birth_date=birthDate, 
+        gender_id=genderId, username=username, password=encryptedPassword)
+        
+        messages.success(request, 'User successfully saved.')
+        
+        return redirect('/users')
+    else:
+        messages.error(request, 'Password do not match.')
+        return redirect('/user/create')
+    
